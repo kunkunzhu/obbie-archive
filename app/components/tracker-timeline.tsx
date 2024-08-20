964f113c48cd982e7ccac5852a90fc6fad617a0b
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-08-20 17:00:09 -0400

    update timeline tracker component

--- app/components/tracker-timeline.tsx
+++ app/components/tracker-timeline.tsx
@@ -1,32 +1,53 @@
 /** @format */
 
 import { HobbyDataI } from "common/types";
-import { ReactNode } from "react";
+import "remixicon/fonts/remixicon.css";
 
 interface TimelineTrackerI {
   entries: HobbyDataI[];
+  emojiDict: any;
 }
 
 interface TimelineEntryI {
   entry: HobbyDataI;
+  key: number;
+  emoji: string;
 }
 
-function TimelineEntry({ entry }: TimelineEntryI) {
+function TimelineEntry({ entry, key, emoji }: TimelineEntryI) {
+  const { category, date } = entry;
+
+  const className = "timeline-entry " + category;
+
+  // const month = date.getUTCMonth() + 1;
+  const day = date.getUTCDate();
+
   return (
-    <div className="timeline-entry">
+    <div className={className}>
+      <div className="day">{day}</div>
+      <span className="category">{emoji}</span>
       {entry.text && <div className="text">{entry.text}</div>}
     </div>
   );
 }
 
-export default function TimelineTracker({ entries }: TimelineTrackerI) {
+export default function TimelineTracker({
+  entries,
+  emojiDict,
+}: TimelineTrackerI) {
+  function renderEntry({ entry, idx }: { entry: HobbyDataI; idx: number }) {
+    const emoji = emojiDict[entry.category];
+    return <TimelineEntry entry={entry} key={idx} emoji={emoji} />;
+  }
+
   return (
-    <div>
+    <div className="timeline-wrapper">
+      <div className="add-action" onClick={() => alert("ADD")}>
+        <i className="ri-add-line"></i>
+      </div>
       {entries.length > 0 ? (
         <div className="timeline">
-          {entries.map((entry, idx) => (
-            <TimelineEntry entry={entry} key={idx} />
-          ))}
+          {entries.map((entry, idx) => renderEntry({ entry, idx }))}
         </div>
       ) : (
         <div>log an entry here!</div>
