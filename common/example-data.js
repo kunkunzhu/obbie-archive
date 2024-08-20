2a97e643c07740a53a24117ab2fb318a1d95b606
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-08-19 21:38:53 -0400

    add example data

---
+++ common/example-data.js
@@ -0,0 +1,80 @@
+/** @format */
+
+export const hobbyEntriesData = {
+  aug_12: {
+    category: "reading",
+    text: "read little fires everywhere during the hour-long commute",
+    project: ["little fires everywhere"],
+    date: new Date("2024-08-12"),
+    star: false,
+  },
+  aug_11: {
+    category: "coding",
+    text: "worked on some bug fixes for hobby tracker",
+    project: ["hobby tracker"],
+    date: new Date("2024-08-11"),
+    star: false,
+  },
+  aug_10_1: {
+    category: "reading",
+    text: "read little fires everywhere in the morning and astrophysics for people in a hurry before sleep",
+    project: ["little fires everywhere", "astrophysics for people in a hurry"],
+    date: new Date("2024-08-10"),
+    star: false,
+  },
+  aug_10_2: {
+    category: "writing",
+    text: "started a new draft on thoughts on traveling",
+    project: ["thoughts on traveling"],
+    date: new Date("2024-08-10"),
+    star: false,
+  },
+  aug_10_2: {
+    category: "writing",
+    text: "started reading little fires everywhere finallyyyy after having the book for so long",
+    project: ["little fires everywhere"],
+    date: new Date("2024-08-9"),
+    star: true,
+  },
+};
+
+export const timeTable = {
+  months: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+};
+
+export const hobbiesData = [
+  {
+    emoji: "📚",
+    name: "reading",
+    color: "#FFD3BA",
+  },
+  {
+    emoji: "📝",
+    name: "writing",
+    color: "#AEE2CD",
+  },
+  {
+    emoji: "💻",
+    name: "coding",
+    color: "#CFDEFF",
+  },
+  {
+    emoji: "🎨",
+    name: "art",
+    color: "#FFFCCF",
+  },
+];
