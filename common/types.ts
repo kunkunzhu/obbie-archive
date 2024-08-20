37fbf04c766f89d0f6580de51b2f5d67d958de7f
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-08-19 21:37:45 -0400

    init typescript config

---
+++ common/types.ts
@@ -0,0 +1,16 @@
+/** @format */
+
+export interface HobbyDataI {
+  category: string;
+  text?: string;
+  projects?: string[];
+  pictureURL?: string;
+  date: Date;
+  star: boolean;
+}
+
+export interface HobbyI {
+  emoji: string;
+  name: string;
+  color: string;
+}
