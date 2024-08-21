3b4300a21eef327a0597dd0ff58f51aed4a44cf9
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-08-21 19:16:37 -0400

    reorg data struct & setup mock database

---
+++ app/data.ts
@@ -0,0 +1,85 @@
+/** @format */
+
+import { fakeHobbyEntriesData } from "./example-data";
+import { HobbyEntryI, HobbyMutationI } from "./types";
+import invariant from "tiny-invariant";
+
+// FAKE DATABASE as placeholder until backend is created
+
+const fakeHobbyEntries = {
+  entries: {} as Record<string, HobbyEntryI>,
+
+  async getAll(): Promise<HobbyEntryI[]> {
+    return Object.keys(fakeHobbyEntries.entries).map(
+      (key) => fakeHobbyEntries.entries[key]
+    );
+  },
+
+  async get(id: string): Promise<HobbyEntryI | null> {
+    return fakeHobbyEntries.entries[id] || null;
+  },
+
+  async create(values: HobbyMutationI): Promise<HobbyEntryI> {
+    const id = Math.random().toString(36).substring(2, 9); // placeholder ID
+    const newEntry = { id, ...values };
+    fakeHobbyEntries.entries[id] = newEntry;
+    return newEntry;
+  },
+
+  async set(id: string, values: HobbyMutationI): Promise<HobbyEntryI> {
+    const entry = await fakeHobbyEntries.get(id);
+    invariant(entry, `No entry found for ${id}`);
+    const updatedEntry = { ...entry, ...values };
+    fakeHobbyEntries.entries[id] = updatedEntry;
+    return updatedEntry;
+  },
+
+  destroy(id: string): null {
+    delete fakeHobbyEntries.entries[id];
+    return null;
+  },
+};
+
+// Helper functions to be exported
+
+export async function getHobbyEntries(query?: string | null) {
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  let allEntries = await fakeHobbyEntries.getAll();
+
+  if (query) {
+    if (query == "star") {
+      return allEntries.filter((entry) => entry.star);
+    } else {
+      return allEntries.filter((entry) => entry.category == query);
+    }
+  } else {
+    return allEntries;
+  }
+}
+
+export async function createHobbyEntry(values: HobbyMutationI) {
+  const entry = await fakeHobbyEntries.create(values);
+  return entry;
+}
+
+export async function getHobbyEntry(id: string) {
+  return fakeHobbyEntries.get(id);
+}
+
+export async function updateHobbyEntry(id: string, updates: HobbyEntryI) {
+  const entry = await fakeHobbyEntries.get(id);
+  if (!entry) {
+    throw new Error(`No entry found for ${id}`);
+  }
+  return fakeHobbyEntries.set(id, { ...entry, ...updates });
+}
+
+export async function deleteHobbyEntry(id: string) {
+  fakeHobbyEntries.destroy(id);
+}
+
+fakeHobbyEntriesData.forEach((entry) => {
+  fakeHobbyEntries.create({
+    ...entry,
+  });
+});
--- app/types.ts
+++ app/types.ts
@@ -1,12 +1,22 @@
 /** @format */
 
-export interface HobbyDataI {
+export interface DateI {
+  year: number;
+  month: number;
+  day: number;
+}
+
+export interface HobbyMutationI {
   category: string;
   text?: string;
   projects?: string[];
   pictureURL?: string;
-  date: Date;
+  date: DateI;
   star: boolean;
+}
+
+export interface HobbyEntryI extends HobbyMutationI {
+  id: string;
 }
 
 export interface HobbyI {
